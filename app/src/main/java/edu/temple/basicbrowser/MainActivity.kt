256a36b1842a31ec7ff95e90126b02fd97bb814e
Logan Small <lsmall@sleepycat.me>
2026-03-26 11:39:32 -0400

    Make button open browser

--- app/src/main/java/edu/temple/basicbrowser/MainActivity.kt
+++ app/src/main/java/edu/temple/basicbrowser/MainActivity.kt
@@ -20,12 +20,17 @@ class MainActivity : AppCompatActivity() {
         urlEditText = findViewById(R.id.urlEditText)
         goButton = findViewById(R.id.goButton)
         webView = findViewById(R.id.webView)
+        webView.settings.javaScriptEnabled = true
 
         // Allow your browser to intercept hyperlink clicks
         webView.webViewClient = object: WebViewClient() {
             override fun onPageFinished(view: WebView?, url: String?) {
                 super.onPageFinished(view, url)
             }
+        }
+
+        goButton.setOnClickListener {
+            webView.loadUrl(urlEditText.text.toString())
         }
 
     }
